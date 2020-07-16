e7b2523f8d84dcb9e69187a0ec9922248c6fc06f
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 12:30:32 +0200

    Add createPiece function and use

--- tetris.js
+++ tetris.js
@@ -47,6 +47,54 @@ function createMatrix(w, h) {
     return matrix;
 }
 
+//Add createPiece function and use
+
+function createPiece(type)
+{
+    if (type === 'I') {
+        return [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ];
+    } else if (type === 'L') {
+        return [
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 1],
+        ];
+    } else if (type === 'J') {
+        return [
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0],
+        ];
+    } else if (type === 'O') {
+        return [
+            [1, 1],
+            [1, 1],
+        ];
+    } else if (type === 'Z') {
+        return [
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0],
+        ];
+    } else if (type === 'S') {
+        return [
+            [0, 1, 1],
+            [1, 1, 0],
+            [0, 0, 0],
+        ];
+    } else if (type === 'T') {
+        return [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0],
+        ];
+    }
+}
         
 //Wrap matrix drawing in function               
 function drawMatrix(matrix) {
