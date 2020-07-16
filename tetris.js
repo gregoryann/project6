694e289b5e4d555862e22e2c32fa68e113fef819
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 11:04:29 +0200

    Retrieve and fill canvas with black

--- tetris.js
+++ tetris.js
@@ -9,3 +9,10 @@
     <script src="tetris.js"></script>
 </body>
 </html>
+
+//Retrieve and fill canvas with black
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
