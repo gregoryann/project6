a6db01335b2b594b79724bb63b14fef1f1a98cff
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 12:46:07 +0200

    Add score counting

--- tetris.js
+++ tetris.js
@@ -17,6 +17,7 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 function arenaSweep() {
+    let rowCount = 1;
     outer: for (let y = arena.length -1; y > 0; --y) {
         for (let x = 0; x < arena[y].length; ++x) {
             if (arena[y][x] === 0) {
@@ -27,6 +28,9 @@ function arenaSweep() {
         const row = arena.splice(y, 1)[0].fill(0);
         arena.unshift(row);
         ++y;
+        
+        player.score += rowCount * 10;
+        rowCount *= 2;
     }
 }
 
@@ -192,6 +196,8 @@ function playerDrop() {
         merge(arena, player);
         playerReset();
         arenaSweep();
+        updateScore();
+        
     }   
     
     
@@ -218,6 +224,8 @@ function playerReset() {
                    (player.matrix[0].length / 2 | 0);
      if (collide(arena, player)) {
         arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore()
     }
 }
 
@@ -274,6 +282,11 @@ function update(time = 0) {
     
     requestAnimationFrame(update);
 }
+        
+    function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+        
    document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
         playerMove(-1);
@@ -298,6 +311,15 @@ function update(time = 0) {
     '#FFE138',
     '#3877FF',
 ];       
-        
+ 
+const arena = createMatrix(12, 20);
+const player = {
+    pos: {x: 0, y: 0},
+    matrix: null,
+    score: 0,
+};
+
+playerReset();
+updateScore();       
 
 update();
