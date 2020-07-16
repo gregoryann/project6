e46ed29a87b2879b8ab77127f56202ccd135bbbf
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 12:06:45 +0200

    Add rotate function

--- tetris.js
+++ tetris.js
@@ -109,6 +109,28 @@ function merge(arena, player) {
     });
 }
 
+
+//Add rotate function
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
 function playerDrop() {
     player.pos.y++;
     
