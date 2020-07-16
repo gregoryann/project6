bbd82d3a679d6765271630769654f426077eac2e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 11:05:16 +0200

    Change context scale

--- tetris.js
+++ tetris.js
@@ -13,6 +13,7 @@
 //Retrieve and fill canvas with black
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
+context.scale(20, 20);
 
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
