61688dd9afc0acd2098bd833f6ea132a79a95419
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 11:03:53 +0200

    create index.html and empty tetris.js

---
+++ tetris.js
@@ -0,0 +1,11 @@
+//create index.html and empty tetris.js
+
+<html>
+<head>
+    <title>Tetris</title>
+</head>
+<body>
+    <canvas id="tetris" width="240" height="400" />
+    <script src="tetris.js"></script>
+</body>
+</html>
