aa1e034ab6f59e526287cd3c18ca396ad4b0c3fb
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 12:42:31 +0200

    Add arenaSweep

--- tetris.js
+++ tetris.js
@@ -16,6 +16,20 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function arenaSweep() {
+    outer: for (let y = arena.length -1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+    }
+}
+
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -177,6 +191,7 @@ function playerDrop() {
         player.pos.y--;
         merge(arena, player);
         playerReset();
+        arenaSweep();
     }   
     
     
